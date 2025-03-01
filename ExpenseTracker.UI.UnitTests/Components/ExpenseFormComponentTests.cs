c95065831a43f25405535ee5f3563fca4ca65c36
EssentialProgrammingKnowledge <kamil.wojtasinski.poczta@gmail.com>
2025-03-01 16:38:18 +0100

    [ExpenseTracker] [Frontend] [Exercise1] Solution

--- ExpenseTracker.UI.UnitTests/Components/ExpenseFormComponentTests.cs
+++ ExpenseTracker.UI.UnitTests/Components/ExpenseFormComponentTests.cs
@@ -1,10 +1,246 @@
 ﻿using Bunit;
+using ExpenseTracker.UI.Components;
+using ExpenseTracker.UI.Models;
 using ExpenseTracker.UI.UnitTests.Common;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
+using Shouldly;
+using System.Globalization;
 
 namespace ExpenseTracker.UI.UnitTests.Components
 {
     public class ExpenseFormComponentTests
     {
+        [Fact]
+        public async Task ShouldRenderComponent()
+        {
+            // Arrange Act
+            var result = await CreateComponent();
+
+            result.ExpenseFormComponent.ShouldNotBeNull();
+            result.ExpenseFormComponent.Markup.ShouldNotBeNullOrWhiteSpace();
+            result.DialogReference.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task FillForm_ExpenseIsProvided_ShouldPopulateFields()
+        {
+            // Arrange
+            var existingExpense = new ExpenseDTO
+            {
+                Id = 1,
+                Amount = 200,
+                CategoryId = 1,
+                Description = "Description"
+            };
+            var parameters = new DialogParameters { ["Expense"] = existingExpense };
+
+            // Act
+            var createdComponent = await CreateComponent("Edytuj wydatek", parameters);
+
+            // Assert
+            var descriptionField = createdComponent.ExpenseFormComponent.Find("input[data-name=\"expense-form-description\"]");
+            var categoryIdField = createdComponent.ExpenseFormComponent.Find("input[data-name=\"expense-form-category\"]");
+            var amountField = createdComponent.ExpenseFormComponent.Find("input[data-name=\"expense-form-amount\"]");
+            descriptionField.GetAttribute("value").ShouldBe(existingExpense.Description);
+            int.TryParse(categoryIdField.GetAttribute("value"), CultureInfo.InvariantCulture, out var categoryIdChanged).ShouldBeTrue();
+            categoryIdChanged.ShouldBe(existingExpense.CategoryId);
+            decimal.TryParse(amountField.GetAttribute("value"), CultureInfo.InvariantCulture, out var amountChanged).ShouldBeTrue();
+            amountChanged.ShouldBe(existingExpense.Amount);
+        }
+
+        [Fact]
+        public async Task FillForm_ExpenseProvidedAndInvalidValues_ShouldShowErrors()
+        {
+            // Arrange
+            var existingExpense = new ExpenseDTO
+            {
+                Id = 1,
+                Amount = 200,
+                CategoryId = 2,
+                Description = "Description",
+            };
+            var categories = CreateCategories();
+            var category = categories.First();
+            var parameters = new DialogParameters { ["Expense"] = existingExpense, ["Categories"] = categories };
+            var createdComponent = await CreateComponent(dialogParameters: parameters);
+
+            // Act
+            await FillForm(createdComponent.ExpenseFormComponent, new ExpenseDTO { Amount = 0, Description = string.Empty, CategoryId = 0 });
+
+            // Assert
+            var editForm = createdComponent.ExpenseFormComponent.FindComponent<EditForm>();
+            editForm.ShouldNotBeNull();
+            editForm.Instance.EditContext.ShouldNotBeNull();
+            var validationMessages = editForm.Instance.EditContext.GetValidationMessages();
+            validationMessages.ShouldNotBeNull().ShouldNotBeEmpty();
+        }
+
+        [Fact]
+        public async Task Cancel_ShouldCloseDialogWithoutSaving()
+        {
+            // Arrange
+            var createdComponent = await CreateComponent();
+
+            // Act
+            createdComponent.ExpenseFormComponent.Find("button[data-name=\"expense-form-cancel\"]").Click();
+
+            // Assert
+            var result = await createdComponent.DialogReference.Result;
+            result.ShouldNotBeNull();
+            result.Canceled.ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task SubmitForm_ExpenseNotProvidedAndEmptyForm_ShouldShowErrors()
+        {
+            // Arrange
+            var createdComponent = await CreateComponent();
+
+            // Act
+            createdComponent.ExpenseFormComponent.Find("button[data-name=\"expense-form-submit\"]").Click();
+
+            // Assert
+            var editForm = createdComponent.ExpenseFormComponent.FindComponent<EditForm>();
+            editForm.ShouldNotBeNull();
+            editForm.Instance.EditContext.ShouldNotBeNull();
+            var validationMessages = editForm.Instance.EditContext.GetValidationMessages();
+            validationMessages.ShouldNotBeNull().ShouldNotBeEmpty();
+        }
+
+        [Fact]
+        public async Task SubmitForm_ExpenseProvidedAndInvalidValues_ShouldShowErrors()
+        {
+            // Arrange
+            var existingExpense = new ExpenseDTO
+            {
+                Id = 1,
+                Amount = 200,
+                CategoryId = 2,
+                Description = "Description",
+            };
+            var categories = CreateCategories();
+            var category = categories.First();
+            var parameters = new DialogParameters { ["Expense"] = existingExpense, ["Categories"] = categories };
+            var createdComponent = await CreateComponent(dialogParameters: parameters);
+
+            // Act
+            await FillFormAndSubmit(createdComponent.ExpenseFormComponent, new ExpenseDTO { Amount = 0, Description = string.Empty, CategoryId = 0 });
+
+            // Assert
+            var editForm = createdComponent.ExpenseFormComponent.FindComponent<EditForm>();
+            editForm.ShouldNotBeNull();
+            editForm.Instance.EditContext.ShouldNotBeNull();
+            var validationMessages = editForm.Instance.EditContext.GetValidationMessages();
+            validationMessages.ShouldNotBeNull().ShouldNotBeEmpty();
+        }
+
+        [Fact]
+        public async Task SubmitForm_UserModifiesData_ShouldUpdateValues()
+        {
+            // Arrange
+            var categories = CreateCategories();
+            var category = categories.First();
+            var parameters = new DialogParameters { ["Categories"] = categories };
+            var createdComponent = await CreateComponent("Edytuj wydatek", parameters);
+            var newExpense = new ExpenseDTO
+            {
+                Id = 1,
+                Amount = 200,
+                CategoryId = category.Id,
+                Description = "NewDescription",
+            };
+
+            // Act
+            await FillFormAndSubmit(createdComponent.ExpenseFormComponent, newExpense);
+
+            // Assert
+            var result = await createdComponent.DialogReference.Result;
+            result.ShouldNotBeNull();
+            var data = result.Data as ExpenseDTO;
+            data.ShouldNotBeNull();
+            data.Description.ShouldBe(newExpense.Description);
+            data.Amount.ShouldBe(newExpense.Amount);
+            data.CategoryId.ShouldBe(newExpense.CategoryId);
+            createdComponent.ExpenseFormComponent.Markup.ShouldBe(string.Empty);
+        }
+
+        [Fact]
+        public async Task SubmitForm_EditingExpenseUserModifiesData_ShouldUpdateValuesAndCloseDialogWithCorrectData()
+        {
+            // Arrange
+            var existingExpense = new ExpenseDTO
+            {
+                Id = 1,
+                Amount = 200,
+                CategoryId = 2,
+                Description = "Description",
+            };
+            var categories = CreateCategories();
+            var category = categories.First();
+            var parameters = new DialogParameters { ["Expense"] = existingExpense, ["Categories"] = categories };
+            var createdComponent = await CreateComponent("Edytuj wydatek", parameters);
+            var newExpense = new ExpenseDTO
+            {
+                Id = 1,
+                Amount = 200,
+                CategoryId = category.Id,
+                Description = "NewDescription",
+            };
+
+            // Act
+            await FillFormAndSubmit(createdComponent.ExpenseFormComponent, newExpense);
+
+            // Assert
+            var result = await createdComponent.DialogReference.Result;
+            result.ShouldNotBeNull();
+            var data = result.Data as ExpenseDTO;
+            data.ShouldNotBeNull();
+            data.Description.ShouldBe(newExpense.Description);
+            data.Amount.ShouldBe(newExpense.Amount);
+            data.CategoryId.ShouldBe(newExpense.CategoryId);
+            createdComponent.ExpenseFormComponent.Markup.ShouldBe(string.Empty);
+        }
+
+        private async Task FillFormAndSubmit(IRenderedComponent<MudDialogProvider> expenseFormComponent, ExpenseDTO dto)
+        {
+            await FillForm(expenseFormComponent, dto);
+            expenseFormComponent.Find("button[data-name=\"expense-form-submit\"]").Click();
+        }
+
+        private async Task FillForm(IRenderedComponent<MudDialogProvider> expenseFormComponent, ExpenseDTO dto)
+        {
+            expenseFormComponent.Find("input[data-name=\"expense-form-description\"]").Input(new ChangeEventArgs() { Value = dto.Description });
+            expenseFormComponent.Find("input[data-name=\"expense-form-amount\"]").Input(new ChangeEventArgs() { Value = dto.Amount.ToString(new CultureInfo("en-US")) });
+            var selectListComponent = expenseFormComponent.FindComponents<MudSelect<int>>()
+                                                          .First(c => c.Find("[data-name=\"expense-form-category\"]") is not null);
+            await selectListComponent.InvokeAsync(async () =>
+            {
+                await selectListComponent.Instance.ValueChanged.InvokeAsync(dto.CategoryId);
+            });
+        }
+
+        private async Task<(IRenderedComponent<MudDialogProvider> ExpenseFormComponent, IDialogReference DialogReference)> CreateComponent(string? title = null, DialogParameters? dialogParameters = null)
+        {
+            var dialogService = _testContext.Services.GetRequiredService<IDialogService>();
+            var expenseFormComponent = _testContext.RenderComponent<MudDialogProvider>();
+            var dialogReference = dialogParameters is not null ?
+                await dialogService.ShowAsync<ExpenseFormComponent>(title, dialogParameters)
+                : await dialogService.ShowAsync<ExpenseFormComponent>(title);
+            return (expenseFormComponent, dialogReference);
+        }
+
+        private List<CategoryDTO> CreateCategories()
+        {
+            return [
+                new CategoryDTO { Id = 1, Name = "Category#1", Budget = 1000 },
+                new CategoryDTO { Id = 1, Name = "Category#2", Budget = 5000 },
+                new CategoryDTO { Id = 1, Name = "Category#3", Budget = 10000 }
+            ];
+        }
+
         private readonly TestContext _testContext;
 
         public ExpenseFormComponentTests()
--- ExpenseTracker.UI.UnitTests/Components/ExpensesComponentTests.cs
+++ ExpenseTracker.UI.UnitTests/Components/ExpensesComponentTests.cs
@@ -1,17 +1,341 @@
 ﻿using Bunit;
+using ExpenseTracker.UI.Components;
+using ExpenseTracker.UI.Models;
+using ExpenseTracker.UI.Services;
 using ExpenseTracker.UI.UnitTests.Common;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using MudBlazor;
 using Shouldly;
+using System.Globalization;
+using ExpenseTracker.UI.Languages;
 
 namespace ExpenseTracker.UI.UnitTests.Components
 {
     public class ExpensesComponentTests
     {
+        [Fact]
+        public void ShouldRenderComponent()
+        {
+            // Arrange Act
+            var expensesComponent = _testContext.RenderComponent<ExpensesComponent>();
+
+            // Assert
+            expensesComponent.ShouldNotBeNull();
+            expensesComponent.Markup.ShouldNotBeNullOrWhiteSpace();
+        }
+
+
+        [Fact]
+        public void DataIsLoading_ShouldShowLoadingIndicator()
+        {
+            // Arrange
+            var tcs = new TaskCompletionSource<Result<List<ExpenseDTO>>>();
+            _expenseService.Setup(x => x.GetAll()).Returns(tcs.Task);
+
+            // Act
+            var expensesComponent = _testContext.RenderComponent<ExpensesComponent>();
+
+            // Assert
+            var loadingIndicatorComponent = expensesComponent.FindComponent<MudProgressLinear>();
+            loadingIndicatorComponent.ShouldNotBeNull();
+            loadingIndicatorComponent.Markup.ShouldNotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public void DataLoaded_ShouldRenderTable()
+        {
+            // Arrange Act
+            var expensesComponent = _testContext.RenderComponent<ExpensesComponent>();
+
+            // Assert
+            var expensesTable = expensesComponent.FindComponent<MudTable<ExpenseDTO>>();
+            expensesTable.ShouldNotBeNull();
+            expensesTable.Markup.ShouldNotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public void DataLoaded_WithCollection_ShouldRenderTableWithRecords()
+        {
+            // Arrange
+            var expenses = CreateExpenses();
+            _expenseService.Setup(p => p.GetAll()).ReturnsAsync(Result<List<ExpenseDTO>>.Success(expenses));
+
+            // Act
+            var expensesComponent = _testContext.RenderComponent<ExpensesComponent>();
+
+            // Assert
+            var expensesTable = expensesComponent.FindComponent<MudTable<ExpenseDTO>>();
+            expenses.ForEach(p => expensesTable.Find($"[data-name=\"expenses-column-id-{p.Id}\"]").ShouldNotBeNull());
+        }
+
+        [Fact]
+        public void AddButtonClicked_ShouldOpenModalWithExpenseForm()
+        {
+            // Arrange
+            var expensesComponent = _testContext.RenderComponent<ExpensesComponent>();
+            var addButton = expensesComponent.Find("[data-name=\"expenses-add-button\"]");
+            var expenseFormComponent = _testContext.RenderComponent<MudDialogProvider>();
+
+            // Act
+            addButton.Click();
+
+            // Assert
+            expenseFormComponent.ShouldNotBeNull();
+            expenseFormComponent.Markup.ShouldNotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public void AddNewExpense_EmptyForm_ShouldShowErrorMessages()
+        {
+            // Arrange
+            var expensesComponent = _testContext.RenderComponent<ExpensesComponent>();
+            var addButton = expensesComponent.Find("[data-name=\"expenses-add-button\"]");
+            var expenseFormComponent = _testContext.RenderComponent<MudDialogProvider>();
+            addButton.Click();
+            var submitButton = expenseFormComponent.Find("[data-name=\"expense-form-submit\"]");
+
+            // Act
+            submitButton.Click();
+
+            // Assert
+            var editForm = expenseFormComponent.FindComponent<EditForm>();
+            editForm.ShouldNotBeNull();
+            editForm.Instance.ShouldNotBeNull();
+            editForm.Instance.EditContext.ShouldNotBeNull();
+            var validationMessages = editForm.Instance.EditContext.GetValidationMessages();
+            validationMessages.ShouldNotBeNull().ShouldNotBeEmpty();
+        }
+
+        [Fact]
+        public async Task AddNewExpense_FillForm_ShouldAddAndFetchAllExpenses()
+        {
+            // Arrange
+            var categories = CreateCategories();
+            _categoryService.Setup(c => c.GetAll()).ReturnsAsync(categories);
+            _expenseService.Setup(p => p.Add(It.IsAny<ExpenseDTO>()))
+                .ReturnsAsync(Result.Success());
+            var expensesComponent = _testContext.RenderComponent<ExpensesComponent>();
+            var addButton = expensesComponent.Find("[data-name=\"expenses-add-button\"]");
+            var expenseFormComponent = _testContext.RenderComponent<MudDialogProvider>();
+            addButton.Click();
+            var newExpense = new ExpenseDTO
+            {
+                Description = "NewExpense",
+                Amount = 100,
+                CategoryId = 1
+            };
+            await FillForm(expenseFormComponent, newExpense);
+            var submitButton = expenseFormComponent.Find("[data-name=\"expense-form-submit\"]");
+
+            // Act
+            submitButton.Click();
+
+            // Assert
+            _expenseService.Verify(p => p.Add(It.Is<ExpenseDTO>(p => p.Description == newExpense.Description && p.Amount == newExpense.Amount && p.CategoryId == newExpense.CategoryId)), Times.Once());
+            _expenseService.Verify(p => p.GetAll(), Times.Exactly(2));
+            expenseFormComponent.Markup.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void EditButtonClicked_ShouldOpenModalWithExpenseForm()
+        {
+            // Arrange
+            var categories = CreateCategories();
+            _categoryService.Setup(c => c.GetAll()).ReturnsAsync(categories);
+            var expenses = CreateExpenses();
+            _expenseService.Setup(p => p.GetAll()).ReturnsAsync(Result<List<ExpenseDTO>>.Success(expenses));
+            var firstExpense = expenses.First();
+            var category = CreateCategories().First(c => c.Id == firstExpense.CategoryId);
+            _expenseService.Setup(p => p.GetById(firstExpense.Id)).ReturnsAsync(Result<ExpenseDetailsDTO?>.Success(MapToDetailsDto(firstExpense, category)));
+            var expensesComponent = _testContext.RenderComponent<ExpensesComponent>();
+            var editButton = expensesComponent.Find($"[data-name=\"expenses-column-edit-with-id-{firstExpense.Id}\"]");
+            var expenseFormComponent = _testContext.RenderComponent<MudDialogProvider>();
+
+            // Act
+            editButton.Click();
+
+            // Assert
+            expenseFormComponent.ShouldNotBeNull();
+            expenseFormComponent.Markup.ShouldNotBeNullOrWhiteSpace();
+            _expenseService.Verify(p => p.GetById(firstExpense.Id), Times.Once());
+            var expenseDescriptionInput = expenseFormComponent.Find("input[data-name=\"expense-form-description\"]");
+            var expenseAmountInput = expenseFormComponent.Find("input[data-name=\"expense-form-amount\"]");
+            var expenseCategoryInput = expenseFormComponent.Find("input[data-name=\"expense-form-category\"]");
+            expenseDescriptionInput.GetAttribute("value").ShouldBe(firstExpense.Description);
+            expenseAmountInput.GetAttribute("value").ShouldBe(firstExpense.Amount.ToString(CultureInfo.InvariantCulture));
+            expenseCategoryInput.GetAttribute("value").ShouldBe(firstExpense.CategoryId.ToString());
+        }
+
+        [Fact]
+        public async Task EditExpense_InvalidValues_ShouldShowErrorMessages()
+        {
+            // Arrange
+            var categories = CreateCategories();
+            _categoryService.Setup(c => c.GetAll()).ReturnsAsync(categories);
+            var expenses = CreateExpenses();
+            _expenseService.Setup(p => p.GetAll()).ReturnsAsync(Result<List<ExpenseDTO>>.Success(expenses));
+            var firstExpense = expenses.First();
+            var category = CreateCategories().First(c => c.Id == firstExpense.CategoryId);
+            _expenseService.Setup(p => p.GetById(firstExpense.Id)).ReturnsAsync(Result<ExpenseDetailsDTO?>.Success(MapToDetailsDto(firstExpense, category)));
+            var expensesComponent = _testContext.RenderComponent<ExpensesComponent>();
+            var editButton = expensesComponent.Find($"[data-name=\"expenses-column-edit-with-id-{firstExpense.Id}\"]");
+            var expenseFormComponent = _testContext.RenderComponent<MudDialogProvider>();
+            var newExpense = new ExpenseDTO
+            {
+                Description = string.Empty,
+                Amount = -100,
+                CategoryId = -1
+            };
+            editButton.Click();
+
+            // Act
+            await FillForm(expenseFormComponent, newExpense);
+
+            // Assert
+            _expenseService.Verify(p => p.GetById(firstExpense.Id), Times.Once());
+            var editForm = expenseFormComponent.FindComponent<EditForm>();
+            editForm.ShouldNotBeNull();
+            editForm.Instance.ShouldNotBeNull();
+            editForm.Instance.EditContext.ShouldNotBeNull();
+            var validationMessages = editForm.Instance.EditContext.GetValidationMessages();
+            validationMessages.ShouldNotBeNull().ShouldNotBeEmpty();
+        }
+
+        [Fact]
+        public async Task EditExpense_ValidData_ShouldUpdateAndFetchAllExpenses()
+        {
+            // Arrange
+            var categories = CreateCategories();
+            var expenses = CreateExpenses();
+            _categoryService.Setup(c => c.GetAll()).ReturnsAsync(categories);
+            _expenseService.Setup(p => p.GetAll()).ReturnsAsync(Result<List<ExpenseDTO>>.Success(expenses));
+            var firstExpense = expenses.First();
+            var category = categories.First(c => c.Id == firstExpense.CategoryId);
+            _expenseService.Setup(p => p.GetById(firstExpense.Id)).ReturnsAsync(Result<ExpenseDetailsDTO?>.Success(MapToDetailsDto(firstExpense, category)));
+            _expenseService.Setup(p => p.Update(It.IsAny<ExpenseDTO>())).ReturnsAsync(Result.Success());
+            var expensesComponent = _testContext.RenderComponent<ExpensesComponent>();
+            var editButton = expensesComponent.Find($"[data-name=\"expenses-column-edit-with-id-{firstExpense.Id}\"]");
+            var expenseFormComponent = _testContext.RenderComponent<MudDialogProvider>();
+            editButton.Click();
+            var newExpense = new ExpenseDTO
+            {
+                Description = "NewEexpense",
+                Amount = 150.25M,
+                CategoryId = 1
+            };
+            await FillForm(expenseFormComponent, newExpense);
+            var submitButton = expenseFormComponent.Find("[data-name=\"expense-form-submit\"]");
+
+            // Act
+            submitButton.Click();
+
+            // Assert
+            _expenseService.Verify(p => p.GetById(firstExpense.Id), Times.Once());
+            _expenseService.Verify(p => p.Update(It.Is<ExpenseDTO>(p => p.Description == newExpense.Description && p.Amount == newExpense.Amount && p.CategoryId == newExpense.CategoryId)), Times.Once());
+            _expenseService.Verify(p => p.GetAll(), Times.Exactly(2));
+            expenseFormComponent.Markup.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void DeleteButtonClicked_ShouldOpenModal()
+        {
+            // Arrange
+            var expenses = CreateExpenses();
+            _expenseService.Setup(p => p.GetAll()).ReturnsAsync(Result<List<ExpenseDTO>>.Success(expenses));
+            var firstExpense = expenses.First();
+            var category = CreateCategories().First(c => c.Id == firstExpense.CategoryId);
+            _expenseService.Setup(p => p.GetById(firstExpense.Id)).ReturnsAsync(Result<ExpenseDetailsDTO?>.Success(MapToDetailsDto(firstExpense, category)));
+            var expensesComponent = _testContext.RenderComponent<ExpensesComponent>();
+            var deleteButton = expensesComponent.Find($"[data-name=\"expenses-column-delete-with-id-{firstExpense.Id}\"]");
+            var deleteexpenseComponent = _testContext.RenderComponent<MudDialogProvider>();
+
+            // Act
+            deleteButton.Click();
+
+            // Assert
+            deleteexpenseComponent.ShouldNotBeNull();
+            deleteexpenseComponent.Markup.ShouldNotBeNullOrWhiteSpace();
+            var yesButton = deleteexpenseComponent.Find(".mud-message-box__yes-button");
+            yesButton.ShouldNotBeNull();
+            var noButton = deleteexpenseComponent.Find(".mud-message-box__no-button");
+            noButton.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void DeleteExpense_ShouldDeleteAndFetchAllExpenses()
+        {
+            // Arrange
+            var expenses = CreateExpenses();
+            _expenseService.Setup(p => p.GetAll()).ReturnsAsync(Result<List<ExpenseDTO>>.Success(expenses));
+            var firstExpense = expenses.First();
+            _expenseService.Setup(p => p.Delete(firstExpense.Id)).ReturnsAsync(Result.Success());
+            var expensesComponent = _testContext.RenderComponent<ExpensesComponent>();
+            var deleteButton = expensesComponent.Find($"[data-name=\"expenses-column-delete-with-id-{firstExpense.Id}\"]");
+            var deleteexpenseComponent = _testContext.RenderComponent<MudDialogProvider>();
+            deleteButton.Click();
+            var confirmDeleteButton = deleteexpenseComponent.Find(".mud-message-box__yes-button");
+
+            // Act
+            confirmDeleteButton.Click();
+
+            // Assert
+            deleteexpenseComponent.Markup.ShouldBeEmpty();
+            _expenseService.Verify(p => p.Delete(firstExpense.Id), Times.Once());
+            _expenseService.Verify(p => p.GetAll(), Times.Exactly(2));
+        }
+
+        private async Task FillForm(IRenderedComponent<MudDialogProvider> expenseFormComponent, ExpenseDTO dto)
+        {
+            expenseFormComponent.Find("input[data-name=\"expense-form-description\"]").Input(new ChangeEventArgs() { Value = dto.Description });
+            expenseFormComponent.Find("input[data-name=\"expense-form-amount\"]").Input(new ChangeEventArgs() { Value = dto.Amount.ToString(new CultureInfo("en-US")) });
+            var selectListComponent = expenseFormComponent.FindComponents<MudSelect<int>>()
+                                                          .First(c => c.Find("[data-name=\"expense-form-category\"]") is not null);
+            await selectListComponent.InvokeAsync(async () =>
+            {
+                await selectListComponent.Instance.ValueChanged.InvokeAsync(dto.CategoryId);
+            });
+        }
+
+        private ExpenseDetailsDTO MapToDetailsDto(ExpenseDTO dto, CategoryDTO category)
+        {
+            return new ExpenseDetailsDTO { Id = dto.Id, Amount = dto.Amount, Description = dto.Description, Category = category };
+        }
+
+        private List<CategoryDTO> CreateCategories()
+        {
+            return [
+                new CategoryDTO { Id = 1, Name = "Category#1", Budget = 1000 },
+                new CategoryDTO { Id = 1, Name = "Category#2", Budget = 5000 },
+                new CategoryDTO { Id = 1, Name = "Category#3", Budget = 10000 }
+            ];
+        }
+
+        private List<ExpenseDTO> CreateExpenses()
+        {
+            return [
+                new ExpenseDTO { Id = 1, Description = "Category#1", Amount = 100, CategoryId = 1 },
+                new ExpenseDTO { Id = 1, Description = "Category#2", Amount = 500, CategoryId = 2 },
+                new ExpenseDTO { Id = 1, Description = "Category#3", Amount = 1000, CategoryId = 3 }
+            ];
+        }
+
         private readonly TestContext _testContext;
+        private readonly Mock<IExpenseService> _expenseService;
+        private readonly Mock<ICategoryService> _categoryService;
 
         public ExpensesComponentTests()
         {
+            _expenseService = new Mock<IExpenseService>();
+            _categoryService = new Mock<ICategoryService>();
             _testContext = new ConfiguredTestContext();
+            _testContext.Services.AddScoped((_) => _expenseService.Object);
+            _testContext.Services.AddScoped((_) => _categoryService.Object);
+            _categoryService.Setup(c => c.GetAll()).ReturnsAsync([]);
+            _expenseService.Setup(c => c.GetAll()).ReturnsAsync(Result<List<ExpenseDTO>>.Success([]));
+            _testContext.Services.AddSingleton<ITranslateService, TranslateService>();
             var popoverProvider = _testContext.RenderComponent<MudPopoverProvider>();
             popoverProvider.ShouldNotBeNull();
             popoverProvider.Markup.ShouldNotBeNull();
