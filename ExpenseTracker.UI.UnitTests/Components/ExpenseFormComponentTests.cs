cd939432ea68bc12da8ced3993fc828b04bae4f9
EssentialProgrammingKnowledge <kamil.wojtasinski.poczta@gmail.com>
2025-03-01 14:21:31 +0100

    [ExpenseTracker] [Frontend] [Exercise1] Prepare

---
+++ ExpenseTracker.UI.UnitTests/Components/ExpenseFormComponentTests.cs
@@ -0,0 +1,15 @@
+﻿using Bunit;
+using ExpenseTracker.UI.UnitTests.Common;
+
+namespace ExpenseTracker.UI.UnitTests.Components
+{
+    public class ExpenseFormComponentTests
+    {
+        private readonly TestContext _testContext;
+
+        public ExpenseFormComponentTests()
+        {
+            _testContext = new ConfiguredTestContext();
+        }
+    }
+}
---
+++ ExpenseTracker.UI.UnitTests/Components/ExpensesComponentTests.cs
@@ -0,0 +1,20 @@
+﻿using Bunit;
+using ExpenseTracker.UI.UnitTests.Common;
+using MudBlazor;
+using Shouldly;
+
+namespace ExpenseTracker.UI.UnitTests.Components
+{
+    public class ExpensesComponentTests
+    {
+        private readonly TestContext _testContext;
+
+        public ExpensesComponentTests()
+        {
+            _testContext = new ConfiguredTestContext();
+            var popoverProvider = _testContext.RenderComponent<MudPopoverProvider>();
+            popoverProvider.ShouldNotBeNull();
+            popoverProvider.Markup.ShouldNotBeNull();
+        }
+    }
+}
